232b8dc8f9c0380d06814006f9d29f9c83ae08c9
hidekicchan <naniwaentertainment@gmail.com>
2021-01-24 22:31:16 +0900

    Prefectureモデル作成(ActiveHash)

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,3 +1,20 @@
 class Item < ApplicationRecord
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to_active_hash :prefecture
   belongs_to :user
+
+  with_options presence: true do
+    validates :name
+    validates :description
+    validates :price, format: { with: /\A[0-9]+\z/i }, numericality: { greater_than_or_equal_to: 300, less_than_or_equal_to: 9_999_999 }
+    validates :image
+  end
+  
+  with_options presence: true, numericality: { other_than: 0, message: 'Select' } do
+    validates :category_id
+    validates :status_id
+    validates :shipping_fee_id
+    validates :prefecture_id
+    validates :shipping_date_id
+  end
 end
