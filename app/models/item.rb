4f0ddf5c17cd16e461e1526bd051596471504789
hidekicchan <naniwaentertainment@gmail.com>
2021-01-24 22:56:08 +0900

    Shipping_feeモデル作成(ActiveHash))

--- app/models/item.rb
+++ app/models/item.rb
@@ -2,6 +2,8 @@ class Item < ApplicationRecord
   extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to_active_hash :prefecture
   belongs_to_active_hash :category
+  belongs_to_active_hash :status
+  belongs_to_active_hash :shipping_fee
   belongs_to :user
 
   with_options presence: true do
