7486d0b22ec47def85a9718f970489056d3aded2
hidekicchan <naniwaentertainment@gmail.com>
2021-01-25 09:59:49 +0900

    Active Storageの導入

--- app/models/item.rb
+++ app/models/item.rb
@@ -5,6 +5,7 @@ class Item < ApplicationRecord
   belongs_to_active_hash :status
   belongs_to_active_hash :shipping_fee
   belongs_to_active_hash :shipping_date
+  has_one_attached :image
   belongs_to :user
 
   with_options presence: true do
